3e3ce4869858d16816070d4c07db83212cd5d5e5
Aghafardeenkhan <133490015+Aghafardeenkhan@users.noreply.github.com>
2023-05-14 11:51:19 +0500

    Create spinlock.c

---
+++ spinlock.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+
+#define SPINLOCK_INIT 0
+#define SPINLOCK_LOCKED 1
+void *thread_func(void *arg);
+static unsigned int spinlock = SPINLOCK_INIT;
+void *thread_func(void *arg);
+int main()
+{
+    pthread_t thread_id[10];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        pthread_create(&thread_id[i], NULL, thread_func, NULL);
+    }
+
+    for (i = 0; i < 5; i++) {
+        pthread_join(thread_id[i], NULL);
+    }
+
+    return 0;
+}
+
+void *thread_func(void *arg)
+{
+    while (syscall(335, &spinlock)) {
+        // spin until lock is acquired
+    }
+
+    printf("Thread %ld acquired the spinlock\n", syscall(SYS_gettid));
+
+    // perform some operations here
+
+    //syscall(335, &spinlock);
+    printf("Thread %ld released the spinlock\n", syscall(SYS_gettid));
+
+    pthread_exit(NULL);
+}
